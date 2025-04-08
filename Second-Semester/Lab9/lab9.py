a5e795634d6376dc22d3ec35a954a5258d3ef745
bllryy <GoschaF26@chcstudent.com>
2025-04-08 00:15:49 -0400

    commit

--- Second-Semester/Lab9/lab9.py
+++ Second-Semester/Lab9/lab9.py
@@ -1,3 +1,4 @@
+
 """
 Henry Lynch, Dylan Mellendick, Marcus Kozlowski, Fin Goscha, & Jaylen Clemons
 
@@ -20,26 +21,60 @@ def loadBoard(filename,board):
         board = [list(line.strip()) for line in file if line.strip()]
     return board
 
+"""
+BASECASE
 
+if we're at the bottom right
+    return score
 
+RECURSIVE CASE
+
+1)Find first occurrence of a B or W <- Determines current player
+2) Recursively go across the row to find how long the Top row of a perimeter is
+3) Check the next row to see if its a middle row and count the empty spaces if it is
+4)The next row is either a top row or a middle row and just repeat (2 and 3)
+5) after you find a perimeter and have scored it, flip back to 1) but start where the perimeter ends and start over from 1)
+"""
 def scoreGame(board,current_player,score,x,y):
-    white_score = 0
-    black_score = 0
-
-    # go with linear search or a bfs solution
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if space == 'B':
-                black_score += 1
-            elif cell == 'W':
-                white_score += 1
-            elif cell == '.':
-                # check for the spaces around the current pos1
-
-    
+    if x == len(board)-1 and y == len(board[0])-1:
+        return score
+   
+    if board[x]=="B" or "W":
+        board[x]=board[x+1]
 
 
+    if board[x][y]== "B":
+        scoreGame(board,current_player,score,x+1,y)
+        scoreGame(board,current_player,score,x,y+1)
+    if board[x][y]== "W":
+       
+def switchPlayer(current_player):
+    return 'W' if current_player == 'B' else 'B'
 
+
+
+def floodfill(x,y, color , visited, board):
+    # check for the out of bounds
+    # check for already visitied
+    # found a dot
+    # diff color
+    if x < 0 or x>= len(board) or y < 0 or y >= len(board[0])
+
+    if (x,y) is  visited:
+        return True
+    # TODO NEED SOMTHIG BETWEEN THE RETURN AND THTE BOOL like a group of the stuff
+    if board[x][y] == '.':
+        return True  # a dot
+
+    if board[x][y] != color:
+        return False  # dif color
+
+
+
+
+
+
+   
 def printBoard(board):
     for row in board:
         print("", end="")
@@ -51,14 +86,5 @@ def main():
     BOARD=loadBoard("testcase1")
     printBoard(BOARD)
 
-    
-
-
-
-
-
-
-
 if __name__ == "__main__":
     main()
-
