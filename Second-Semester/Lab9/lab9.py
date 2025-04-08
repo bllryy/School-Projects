3680a35f6fda0e5fcb1ed5680763acf7946b685c
bllryy <GoschaF26@chcstudent.com>
2025-04-08 07:46:24 -0400

    commit

--- Second-Semester/Lab9/lab9.py
+++ Second-Semester/Lab9/lab9.py
@@ -34,7 +34,11 @@ RECURSIVE CASE
 3) Check the next row to see if its a middle row and count the empty spaces if it is
 4)The next row is either a top row or a middle row and just repeat (2 and 3)
 5) after you find a perimeter and have scored it, flip back to 1) but start where the perimeter ends and start over from 1)
+
 """
+
+
+# TODO: feeling two for loops to go though every cell on the board
 def scoreGame(board,current_player,score,x,y):
     if x == len(board)-1 and y == len(board[0])-1:
         return score
@@ -47,7 +51,10 @@ def scoreGame(board,current_player,score,x,y):
         scoreGame(board,current_player,score,x+1,y)
         scoreGame(board,current_player,score,x,y+1)
     if board[x][y]== "W":
-       
+
+
+
+        
 def switchPlayer(current_player):
     return 'W' if current_player == 'B' else 'B'
 
@@ -69,9 +76,40 @@ def floodfill(x,y, color , visited, board):
     if board[x][y] != color:
         return False  # dif color
 
+    group = {(x,y)}
+    has_dot = False
+
+    """
+    ALSO A SET COULD WORK
+    
+    check for all four directions from the current x,y
+    collect all connected stones of the same color
+    check if any of them have a dot or a liberity
+
+    start with the all four directions d
+    calc the neighbors cords n
+    then recursively continue from that neighbor
+    add to a group of connected stones
+    and keep track of weather any part of the whole group as a empty space
+
+    return the group of the connected stones and if they have a empty space
+    """
+    
+
+    for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]: # hold these here for now might need to change later
+        nx, ny = x + dx, y + dy
+        new_group, has_no_space = floodfill(nx, ny, color, visited, board)
+        group.update(new_group)
+        # https://stackoverflow.com/questions/67573293/pygame-tutorial-tank-with-collision-detection
+        # https://www.w3schools.com/python/ref_set_update.asp
+
+        #has_no_space = has_no_space or 
+    
+    return group, has_no_space
 
 
 
+    
 
 
    
@@ -83,8 +121,16 @@ def printBoard(board):
         print()
 
 def main():
-    BOARD=loadBoard("testcase1")
-    printBoard(BOARD)
+    board = loadBoard("testcase1")
+    score = = scoreGame(board)
 
+    print("\nBoard after the capture:")
+    printBoard(board)
+
+    print("\nCaptured stones:")
+    print("Black captured", score['W'], "white stones.")
+    print("White captured", score['B'], "black stones.")
+
+    
 if __name__ == "__main__":
     main()
