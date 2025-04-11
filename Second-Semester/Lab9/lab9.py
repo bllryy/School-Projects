c4454184e4edda108540ae1ab4466e461fba6ed6
bllryy <GoschaF26@chcstudent.com>
2025-04-11 10:49:26 -0400

    commit

--- Second-Semester/Lab9/lab9.py
+++ Second-Semester/Lab9/lab9.py
@@ -1,6 +1,6 @@
 
 """
-Henry Lynch, Dylan Mellendick, Marcus Kozlowski, Fin Goscha, & Jaylen Clemons
+Henry Lynch, Dylan Mellendick, Fin Goscha, & Jaylen Clemons
 
 Lab 8
 """
@@ -68,19 +68,30 @@ Score game
 def switchPlayer(current_player):
     return 'W' if current_player == 'B' else 'B'
 
-def find_inside(board, color):
+def find_inside(board):
         for x  in range(len(board)):
             for y in range (len(board[0])):
-                    if board [x][y] == color:
-                        if board [x+1][y] == color:
+                    if board [x][y] == "B" and board[x][y+1] == "B":
+                        if board [x+1][y] == "B":
                             if board [x+1][y+1] == '.':
-                                return [x+1,y +1]
-                        if board [x+1] [y-1] == color:
+                                return [x+1,y +1, "B"]
+                        elif board [x+1] [y-1] == "B":
                             if board [x+1] [y] == '.':
-                                return [x+1,y +1]
-                        if board [x+1] [y+1] == color:
+                                return [x+1,y +1, "B"]
+                        elif board [x+1] [y+1] == "B":
                             if board [x+1] [y+2] == '.':
-                                return [x+1,y+2]
+                                return [x+1,y+2, "B"]
+                    if board [x][y] == "W" and board[x][y+1] == "W":
+                        if board [x+1][y] == "W":
+                            if board [x+1][y+1] == '.':
+                                return [x+1,y+1, "W"]
+                        elif board [x+1] [y-1] == "W":
+                            if board [x+1] [y] == '.':
+                                return [x+1,y +1, "W"]
+                        elif board [x+1] [y+1] == "W":
+                            if board [x+1] [y+2] == '.':
+                                return [x+1,y+2, "W"]
+        return ["Done"]
 
 
 
@@ -92,7 +103,7 @@ def floodfill(matrix, x, y, color):
         #recursively invoke flood fill on all surrounding cells:
         if x > 0:
             floodfill(matrix,x-1,y,color)
-        if x < len(matrix[y]) - 1:
+        if x < len(matrix[x]) - 1:
             floodfill(matrix,x+1,y,color)
         if y > 0:
             floodfill(matrix,x,y-1,color)
@@ -100,11 +111,16 @@ def floodfill(matrix, x, y, color):
             floodfill(matrix,x,y+1,color)
 
 
-    
-
-def sccoreGame(board):
-    pass
-   
+def countScore(board):
+    blackScore= 0
+    whiteScore= 0
+    for row in board:
+        for cell in row:
+            if cell == "B":
+                blackScore+=1
+            if cell == "W":
+                whiteScore+=1
+    return [blackScore, whiteScore]
 def printBoard(board):
     for row in board:
         print("", end="")
@@ -113,20 +129,23 @@ def printBoard(board):
         print()
 
 def main():
-    board = loadBoard("testcase.txt")
-    
-    #score = scoreGame(board)
-
+    board = loadBoard("testcase5.txt")
+    printBoard(board)
     print("\nBoard after the capture:")
+    coords = find_inside(board)
+    while coords[0] != "Done":
+        floodfill(board, coords[0], coords[1], coords[2])
+        coords=find_inside(board)
     printBoard(board)
-    coords = find_inside(board, 'B')
-    print(board[coords[0]][coords[1]])
-    floodfill(board, coords[0], coords[1] , 'B')
-    print("\nCaptured stones:")
-    printBoard(board)
-    #print("Black captured", score['W'], "white stones.")
-    #print("White captured", score['B'], "black stones.")
-
+    score=countScore(board)
+    print("Black captured", score[0], "black stones.")
+    print("White captured", score[1], "white stones.")
+    if score[0] > score[1]:
+        print("Black wins")
+    elif score[1]> score[0]:
+        print("White wins")
+    else:
+        print("Tie")
     
 if __name__ == "__main__":
     main()
